bde33dd500522fa313c38513e0641434553ed173
algazza <145945668+algazza@users.noreply.github.com>
2024-02-27 15:55:17 +0700

    Update data.js

--- data.js
+++ data.js
@@ -4,6 +4,6 @@ const qrcode = document.getElementById("qr-code");
 (function () {
   let qr = new QRious({
     element: qrcode,
-    value: `http://192.168.77.143/buku-tamu-backend/getData.php?name=${params.get("name")}&kategori=${params.get("kategori")}&jabatan=${params.get("jabatan")}&no=${params.get("no")}`,
+    value: `http://192.168.222.43/buku-tamu-backend/getData.php?name=${params.get("name")}&kategori=${params.get("kategori")}&jabatan=${params.get("jabatan")}&no=${params.get("no")}`,
   });
 })();
